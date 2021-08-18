1e63807aa675378f08a3a9abf8bd10c609f771ed
noah <noah071610@naver.com>
2021-08-19 01:18:52 +0900

    Fix : Emotin & twin.macro SSR styling and Mapbox viewport

---
+++ client/pages/_document.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import { extractCritical } from "@emotion/server";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const page = await ctx.renderPage();
+    const styles = extractCritical(page.html);
+    return {
+      ...initialProps,
+      ...page,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style
+            data-emotion-css={styles.ids.join(" ")}
+            dangerouslySetInnerHTML={{ __html: styles.css }}
+          />
+        </>
+      ),
+    };
+  }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
